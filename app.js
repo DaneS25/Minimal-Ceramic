47396a9088d5882ebe86cb4eaa0e25a9b6d32f54
DaneS25 <71624904+DaneS25@users.noreply.github.com>
2021-05-30 18:42:57 +1200

    algined text in index, added fader to pages

---
+++ app.js
@@ -0,0 +1,27 @@
+var start = 200;
+
+window.onscroll = function () {
+    scrollRotate();
+};
+function scrollRotate() {
+    console.log(window.pageYOffset/2);
+    let image2 = document.getElementById("rotate2");
+    let image = document.getElementById("rotate1");
+
+    if (window.pageYOffset < start){
+        image2.style.transform = "rotate(" + (start/4-window.pageYOffset/4) + "deg)";
+        image.style.transform = "rotate(" + (-start/4+window.pageYOffset/4) + "deg)";
+        image.style.marginLeft =  -window.pageYOffset*1.4 + "px";
+        image.style.marginRight =  window.pageYOffset*2 + "px";
+        image.style.marginTop =  window.pageYOffset + "px";
+
+
+        image2.style.marginRight =  -window.pageYOffset*1.4 + "px";
+        image2.style.marginLeft =  window.pageYOffset*2 + "px";
+        image2.style.marginTop =  window.pageYOffset + "px";
+    }
+    else{
+        image.style.transform = "rotate(0deg)";
+        image2.style.transform = "rotate(0deg)";
+    }
+}
